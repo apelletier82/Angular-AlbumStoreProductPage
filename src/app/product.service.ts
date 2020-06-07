477ce6028ab0ac56842531c1b7ff0f1a8f2e0a47
Alexandre Pelletier <apelletier82@gmail.com>
2020-06-07 17:11:56 +0200

    Test 6 Displaying a List of Products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,15 +5,22 @@ import 'rxjs/add/operator/map';
 
 import { Album } from './album'; 
 import { AlbumInfo } from './album-info';
+import { Product } from './product';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl: string = '../assets/album.json';
+  private _productsUrl: string = '../assets/products.json';
   
   constructor(private _http: Http) { }
 
   getAlbum(id:number): Observable<AlbumInfo> {
     return this._http.get(this._albumUrl).map(response => <AlbumInfo>response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
 }
